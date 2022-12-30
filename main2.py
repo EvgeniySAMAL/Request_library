e9d6e3344e45d7a9220c54c2438bbf122edef3b0
Samal <evgeniy.samal@mail.ru>
2022-12-30 17:42:09 +0300

    реализованны все запросы

--- main2.py
+++ main2.py
@@ -2,19 +2,44 @@ import requests
 
 # website = "https://jsonplaceholder.typicode.com/posts" # post запрос
 #
-# response = requests.post(website, data={               # post запрос
+# response = requests.post(website, data={
 #     "userid":12,
 #     "title":"my new posts",
 #     "body":"body for my post IT OVERONE",
 #     "photo":{"1.jpg","2.jpg"}
 #
 # })
-# print(response.text)                                   # post запрос
+# print(response.text)
 
 
-website = "https://jsonplaceholder.typicode.com/posts/2"   # put запрос
+# website = "https://jsonplaceholder.typicode.com/posts/2"   # put запрос
+# print(requests.get(website).json())
+# response = requests.put(website, data={
+#     "id":2,
+#     "userid":12,
+#     "title":"this is old posts",
+#     "photo":{"1.jpg","2.jpg"}
+#
+#  })
+#
+# print(response.json())
+
+
+# website = "https://jsonplaceholder.typicode.com/posts/2"   # patch запрос
+# print(requests.patch(website).json())
+# response = requests.put(website, data={
+#     "id":2,
+#     "userid":12,
+#     "title":"this is old posts",
+#
+#  })
+#
+# print(response.json())
+
+
+website = "https://jsonplaceholder.typicode.com/posts/2"   # delete запрос
 print(requests.get(website).json())
-response = requests.put(website, data={
+response = requests.delete(website, data={
     "id":2,
     "userid":12,
     "title":"this is old posts",
@@ -22,4 +47,4 @@ response = requests.put(website, data={
 
  })
 
-print(response.json())
+print(response.status_code)
